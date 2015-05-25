ef71536578f2a84c5706b85ff87fe1c36578c1c0
Marcinn <nawrockim@op.pl>
2015-05-25 22:55:10 +0200

    Missing files for required field

---
+++ cmdLineParserTests/Configs/OneRequiredFieldConfig.cs
@@ -0,0 +1,15 @@
+﻿using cmdLineParser.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cmdLineParserTests.Configs
+{
+    public class OneRequiredFieldConfig
+    {
+        [Required]
+        public String SomeRequiredSetting { get; set; }
+    }
+}
