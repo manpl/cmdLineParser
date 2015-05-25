edb58b40dfe05b4beeadf7d102285a7a2b7ee3b1
Marcinn <nawrockim@op.pl>
2015-05-25 22:52:04 +0200

    Added required field

--- cmdLineParser/Parser.cs
+++ cmdLineParser/Parser.cs
@@ -1,12 +1,74 @@
-﻿using System;
+﻿using cmdLineParser.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace cmdLineParser
 {
-    public class Parser
+    public class Parser<T> where T : new()
     {
+        private T _config;
+        private T Config
+        {
+            get
+            {
+                return _config;
+            }
+            set
+            {
+                _config = value;
+            }
+        }
+
+        public Parser(T config)
+        {
+            if (config == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            Config = config;
+        }
+
+        public T Parse(string input)
+        {
+            var pairs = input.Split(' ')
+                .Select(token => token.Split(':'))
+                .ToDictionary(pair => pair[0], pair => pair[1]);
+
+            foreach (var kp in pairs)
+            {
+                var property = GetProperty(kp.Key);
+
+                if (property != null)
+                {
+                    property.SetValue(Config, kp.Value, null);
+                }
+                else
+                {
+                    // property does not match anything in config
+                }
+            }
+
+            var properties = Config.GetType().GetProperties();
+
+            properties.ToList().ForEach(prop =>
+            {
+                if (prop.GetCustomAttribute<RequiredAttribute>() != null && !pairs.ContainsKey(prop.Name))
+                {
+                    throw new ArgumentException("No value found for " + prop.Name);
+                }
+            });
+
+            return Config;
+        }
+
+        private PropertyInfo GetProperty(string name)
+        {
+            return Config.GetType().GetProperties().SingleOrDefault(prop => prop.Name == name);
+        }
     }
 }
--- cmdLineParserTests/ParserTests.cs
+++ cmdLineParserTests/ParserTests.cs
@@ -1,12 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using cmdLineParser;
+using cmdLineParserTests.Configs;
+using System;
+using Xunit;
 
 namespace cmdLineParserTests
 {
     public class ParserTests
     {
+        [Fact]
+        public void NullConfigPassed_Create_ThrowsException()
+        {
+            Assert.Throws<ArgumentNullException>(() => {
+                new Parser<Object>(null);
+            });
+        }
+
+        [Fact]
+        public void RequiredFieldSupplied_Parse_ValueReturned()
+        {
+            var parser = new Parser<OneRequiredFieldConfig>(new OneRequiredFieldConfig());
+            var result = parser.Parse("SomeRequiredSetting:value");
+            Assert.Equal(result.SomeRequiredSetting, "value");
+        }
+
+        [Fact]
+        public void RedundantOptionSupplied_Parse_OptionSkiped()
+        {
+            var parser = new Parser<Object>(new Object());
+            var result = parser.Parse("SomethingElse:value");
+        }
+
+        [Fact]
+        public void RequiredFieldNotSupplied_Parse_ExceptionThrows()
+        {
+            var parser = new Parser<OneRequiredFieldConfig>(new OneRequiredFieldConfig());
+            Assert.Throws<ArgumentException>(() => parser.Parse("SomethingElse:value"));
+            
+        }
+
+        [Fact]
+        public void DuplicateValuesSupplied_Parse_ExceptionThrows()
+        {
+            var parser = new Parser<Object>(new Object());
+            Assert.Throws<ArgumentException>(() => parser.Parse("SomethingElse:value SomethingElse:value"));
+        }
+
     }
 }
